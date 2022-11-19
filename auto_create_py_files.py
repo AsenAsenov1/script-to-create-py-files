00f70414a7ad08f6221be60ee9aaa6acd906ba45
Asen Asenov <asen991@gmail.com>
2022-11-19 13:14:16 +0200

    Updated: RegEx pattern added

--- auto_create_py_files.py
+++ auto_create_py_files.py
@@ -37,27 +37,12 @@ while True:
 print("Example task names: 01. Storage02. Weapon03. Catalogue04. Town05. Class06. Inventory07. Articles08. "
       "Vehicle✶09. Movie✶")
 print()
-words = input("Paste the names of the tasks copied from judge: ").split()
-words = "_".join(words)
+words = input("Paste the names of the tasks copied from judge: ")
 
-if "✶" in words:
-    words = words.replace("✶", "")
-if "-" in words:
-    words = words.replace("-", "_")
-if "/" in words:
-    words = words.replace("/", "_")
-if "'" in words:
-    words = words.replace("'", "")
+pattern = r"(\d{2}\.\s([A-Za-z!@#$%^&*_\-,;'`]+\s?)*)"
+match = re.findall(pattern, words)
 
-words = words.replace(".", "")
-new_words = re.split("[0-9][0-9]", words)
-new_words.pop(0)
-nums = re.findall("[0-9][0-9]", words)
-
-for i in range(len(new_words)):
-    nums[i] = nums[i] + new_words[i]
-
-final_list = list(map(lambda x: x.lower(), nums))
+final_list = [x[0].lower().replace(" ", "_").replace(".", "",) for x in match]
 
 if specific_dir:
     for word in final_list:
@@ -65,6 +50,7 @@ if specific_dir:
         with open(os.path.join(filepath, f"{word}.py"), 'w') as fp:
             pass
 else:
+    # Creating a file at current location
     for word in final_list:
         fp = open(f'{word}.py', 'x')
         fp.close()
